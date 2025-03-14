e94e29b04f68200ee6f013dd50250d0c72a87cd4
chutis <L191200090@tlalpan.tecnm.mx>
2025-03-14 12:01:14 -0600

    added basic configuration spring security

---
+++ src/main/java/com/example/demo/controller/HiController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.service.HiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+    @Autowired
+    private HiService hiService;
+
+    @GetMapping
+    public String hi() {
+        return hiService.getMessage();
+    }
+}

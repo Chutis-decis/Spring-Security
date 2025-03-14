574c6982b0ca9248e81141f71837b05b58b351ba
chutis <L191200090@tlalpan.tecnm.mx>
2025-03-14 12:26:11 -0600

    agregando cuando quieramos agregar autenticacion a un usuario

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,20 +3,26 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 
-    //Servira cuando quieramos mandar una peticion sin security
+    //Servira cuando quieramos autenticar todo a un usuario
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {http
-            .authorizeHttpRequests(auth -> auth
-                    .anyRequest()
-                    .permitAll()
-            );
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .authenticated() // Todas las solicitudes deben estar autenticadas
+                )
+                .httpBasic(Customizer.withDefaults()); // Habilita la autenticación básica (usuario y contraseña)
         return http.build();
     }
 }
